ed9b3e139acf205e881cdb15fa94c98e55f7a39d
Mark <mrkswrn@gmail.com>
2020-11-08 16:24:34 +0100

    Fixed bug

--- helper/git_backup.py
+++ helper/git_backup.py
@@ -10,7 +10,7 @@ def gitpush(directory):
     """
     commit_out = subprocess.run(["git", "-C", directory, "commit", "-a", "-m", "Update"])
     log(f"Commited: {commit_out}", "GIT")
-    push_out = subprocess.run(["git", "-C", directory, "push", "-u", "origin", "main"])
+    push_out = subprocess.run(["git", "-C", directory, "push", "-u"])
     # git -C ./data push -u origin main
     log(f"Pushed: {push_out}", "GIT")
     return commit_out, push_out
