808ae67a5a0487805a99ef182bd973c95b23fe61
Mark <mrkswrn@gmail.com>
2020-11-08 16:24:05 +0100

    Updated file_creator.py and settings.json

--- helper/file_creator.py
+++ helper/file_creator.py
@@ -12,6 +12,41 @@ def createFiles():
     website_list = ["ad", "dm", "ep", "la"]
     stop = False
 
+    settings_default = {
+        "upload to git": False,
+        "channel_to_post": "test",
+        "test_livestream_message": True,
+        "send_message_to_finn": False,
+        "lecture": 111111111111111111111,
+        "test": 111111111111111111111
+    }
+
+    schedule_default = {
+        "eprog": {
+            "Mon:04:20": "FILLER",
+            "Tue:04:20": "FILLER",
+            "url": "FILLER"
+        },
+        "diskmat": {
+            "Wed:04:20": "FILLER",
+            "Thu:04:20": "FILLER",
+            "url": "FILLER"
+        },
+        "linalg": {
+            "Fri:04:20": "FILLER",
+            "Sat:04:20": "FILLER",
+            "url": "FILLER"
+        },
+        "and": {
+            "Sun:04:20": "FILLER",
+            "url": "FILLER"
+        },
+        "test": {
+            "test": "TEST MESSAGE",
+            "url": "This is the url to the website"
+        }
+    }
+
     # This is the file that stores the token
     if not os.path.exists("../LECTURFIER.json"):
         log("Creating LECTURFIER.json", keyword)
@@ -19,11 +54,10 @@ def createFiles():
         with open("../LECTURFIER.json", "w") as f:
             f.write('{"token":""}')
 
-    if not os.path.exists(f"./data/settings.json"):
-        log(f"Creating settings.json", keyword)
+    if not os.path.exists("./data/settings.json"):
+        log("Creating settings.json", keyword)
         stop = True
-        settings_default = {"upload to git": False}
-        with open(f"./data/settings.json", "w") as f:
+        with open("./data/settings.json", "w") as f:
             json.dump(settings_default, f, indent=2)
 
     for file in file_list:
@@ -36,10 +70,8 @@ def createFiles():
     if not os.path.exists("./data/schedule.json"):
         log("Creating schedule.json", keyword)
         stop = True
-        with open("./data/schedule_default.json", "r") as f:
-            schedule = json.load(f)
         with open("./data/schedule.json", "w") as f:
-            json.dump(schedule, f, indent=2)
+            json.dump(schedule_default, f, indent=2)
 
     for link in website_list:
         if not os.path.exists(f"./websites/{link}.html"):
