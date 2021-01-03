684563ee0b0096cfe4e1680b18a75880bde6c6e6
Mark <mrkswrn@gmail.com>
2021-01-03 20:39:09 +0100

    Moved all major bot things in a new cog 'cause better

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 import json
-from helper import file_creator
-from helper.log import log
 
 with open("./data/settings.json", "r") as f:
     prefix = json.load(f)
@@ -12,110 +10,21 @@ bot = commands.Bot(command_prefix=prefix["prefix"], description='Lecture Notifie
 
 bot.remove_command("help")
 
-file_creator.createFiles()
+# Loads the sub_bot cog, which can then easily be reloaded
+bot.load_extension("cogs.mainbot")
+# Loads the help page, as it has an on_ready event that needs to be called
+bot.load_extension("cogs.help")
 
-startup_extensions = ["player",
-                      "statistics",
-                      "minesweeper",
-                      "hangman",
-                      "quote",
-                      "help",
-                      "reputation",
-                      "admin",
-                      "owner",
-                      "voice_xp",
-                      "lecture_updates"]
-
-
-@bot.command()
-async def reload(ctx, cog=None):
-    """
-    Used to reload a cog. Does not load a cog if the cog is unloaded.
-    :param ctx: The message object
-    :param cog: The name of the cog to load
-    :return: None
-    """
-    if await bot.is_owner(ctx.author):
-        if cog is None:
-            cog_list = '\n'.join(startup_extensions)
-            await ctx.send(f"Following cogs exist:\n"
-                           f"{cog_list}")
-        elif cog in startup_extensions:
-            await ctx.send(await reload_cog(cog))
-            await ctx.send(f"DONE - Reloaded `{cog}`")
-        elif cog == "all":
-            await ctx.send("Reloading all cogs")
-            log("Reloading all cogs", "COGS")
-            for cog in startup_extensions:
-                await ctx.send(await reload_cog(cog))
-            await ctx.send("DONE - Reloaded all cogs")
-        else:
-            await ctx.send(f"Cog does not exist.")
-    else:
-        raise discord.ext.commands.errors.NotOwner
-
-
-async def reload_cog(cog):
-    if await stop_bg_task(cog):
-        msg = "--Stopped background task--"
-    else:
-        msg = "--No background task to stop--"
-    if cog == "player":
-        player = bot.get_cog("Player")
-        player.save("./data/guesses.json")
-        msg += "\n--Saved covid guesses--"
-    bot.reload_extension("cogs." + cog)
-    return f"Reloaded `{cog}`\n{msg}"
-
-
-async def stop_bg_task(task):
-    """
-    Stops the given background task
-    :param task: The task to stop
-    :return: True if the task was stopped, False if no task was stopped
-    """
-    task = task.lower()
-    all_loops = {
-        "lecture_updates": bot.get_cog("Updates").get_task(),
-        "statistics": bot.get_cog("Statistics").get_task(),
-        "voice_xp": bot.get_cog("Voice").get_task(),
-        "player": bot.get_cog("Player").get_task()
-    }
-    if task in all_loops:
-        return all_loops[task].cancel()
-    return False
-
-
-@bot.event
-async def on_ready():
-    log("Logged in as:", "LOGIN")
-    log(f"Name: {bot.user.name}", "LOGIN")
-    log(f"ID: {bot.user.id}", "LOGIN")
-    log(f"Version: {discord.__version__}", "LOGIN")
-    await bot.change_presence(activity=discord.Activity(name='myself just boot up', type=discord.ActivityType.watching))
-    print("-------------")
-
-
-@bot.command()
-async def test(ctx):
-   await ctx.send("reply")
 
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
 
-for extension in startup_extensions:
-    try:
-        loaded_cog = bot.load_extension("cogs." + extension)
-        log("Loaded extension \"{}\".".format(extension), "EXTENSION")
-    except Exception as e:
-        log("Failed loading extension \"{}\"\n-{}: {}".format(extension, e, type(e)), "EXTENSION")
-print("-------------------")
-
+# Load the token
 with open("../LECTURFIERBETA.json", "r") as f:
     settings = json.load(f)
 if len(settings["token"]) == 0:
-    log("NO TOKEN IN LECTURFIER.json! Stopping bot.", "TOKEN")
+    print("NO TOKEN IN LECTURFIER.json! Stopping bot.", "TOKEN")
     exit()
 
 bot.run(settings["token"])
---
+++ cogs/mainbot.py
@@ -0,0 +1,105 @@
+import discord
+from discord.ext import commands
+from helper.log import log
+from helper import file_creator
+
+
+class MainBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.startup_extensions = [
+            "mainbot",
+            "player",
+            "statistics",
+            "minesweeper",
+            "hangman",
+            "quote",
+            "help",
+            "reputation",
+            "admin",
+            "owner",
+            "voice",
+            "updates",
+        ]
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log("\n----------------------------\nStarting up bot")
+        print("Logged in as:", "LOGIN")
+        print(f"Name: {self.bot.user.name}", "LOGIN")
+        print(f"ID: {self.bot.user.id}", "LOGIN")
+        print(f"Version: {discord.__version__}", "LOGIN")
+        await self.bot.change_presence(activity=discord.Activity(name='myself just boot up', type=discord.ActivityType.watching))
+        print("-------------")
+        await self.load_all_extensions(self.startup_extensions)
+        file_creator.createFiles()
+        log("Started up bot\n-------------")
+
+    async def load_all_extensions(self, extensions_to_load):
+        for extension in extensions_to_load:
+            try:
+                loaded_cog = self.bot.load_extension("cogs." + extension)
+                print("Loaded extension \"{}\".".format(extension), "EXTENSION")
+            except discord.ext.commands.errors.ExtensionAlreadyLoaded:
+                pass
+            except Exception as e:
+                print("Failed loading extension \"{}\"\n-{}: {}".format(extension, e, type(e)), "EXTENSION")
+        print("-------------------")
+
+    @commands.command()
+    async def reload(self, ctx, cog=None):
+        """
+        Used to reload a cog. Does not load a cog if the cog is unloaded.
+
+        """
+        if await self.bot.is_owner(ctx.author):
+            if cog is None:
+                cog_list = '\n'.join(self.startup_extensions)
+                await ctx.send(f"Following cogs exist:\n"
+                               f"{cog_list}")
+            elif cog in self.startup_extensions:
+                await ctx.send(await self.reload_cog(cog))
+                await ctx.send(f"DONE - Reloaded `{cog}`")
+            elif cog == "all":
+                await ctx.send("Reloading all cogs")
+                log("Reloading all cogs", "COGS")
+                for cog in self.startup_extensions:
+                    await ctx.send(await self.reload_cog(cog))
+                await ctx.send("DONE - Reloaded all cogs")
+            else:
+                await ctx.send(f"Cog does not exist.")
+        else:
+            raise discord.ext.commands.errors.NotOwner
+
+    async def reload_cog(self, cog):
+        if await self.stop_bg_task(cog):
+            msg = "--Stopped background task--"
+        else:
+            msg = "--No background task to stop--"
+        if cog == "player":
+            player = self.bot.get_cog("Player")
+            player.save("./data/guesses.json")
+            msg += "\n--Saved covid guesses--"
+        self.bot.reload_extension("cogs." + cog)
+        return f"Reloaded `{cog}`\n{msg}"
+
+    async def stop_bg_task(self, task):
+        """
+        Stops the given background task
+        :param task: The task to stop
+        :return: True if the task was stopped, False if no task was stopped
+        """
+        task = task.lower()
+        all_loops = {
+            "lecture_updates": self.bot.get_cog("Updates").get_task(),
+            "statistics": self.bot.get_cog("Statistics").get_task(),
+            "voice_xp": self.bot.get_cog("Voice").get_task(),
+            "player": self.bot.get_cog("Player").get_task()
+        }
+        if task in all_loops:
+            return all_loops[task].cancel()
+        return False
+
+
+def setup(bot):
+    bot.add_cog(MainBot(bot))
