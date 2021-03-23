f92476babb2d0158b1a6a6833a218ae7bf2b6e2e
jackra1n <lok7ok@gmail.com>
2021-03-23 19:47:39 +0100

    fix bot.py, add .gitignore

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import json
 from helper import file_creator
 
+file_creator.createFiles()
+
 with open("./data/settings.json", "r") as f:
     prefix = json.load(f)
 
@@ -10,8 +12,6 @@ intents = discord.Intents()
 bot = commands.Bot(command_prefix=prefix["prefix"], description='Lecture Notifier', intents=intents.all(), owner_id=205704051856244736)
 
 bot.remove_command("help")
-
-file_creator.createFiles()
 
 # Loads the sub_bot cog, which can then easily be reloaded
 bot.load_extension("cogs.mainbot")
@@ -24,7 +24,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 # Load the token
-with open("../LECTURFIERBETA.json", "r") as f:
+with open("../LECTURFIER.json", "r") as f:
     settings = json.load(f)
 if len(settings["token"]) == 0:
     print("NO TOKEN IN LECTURFIER.json! Stopping bot.", "TOKEN")
