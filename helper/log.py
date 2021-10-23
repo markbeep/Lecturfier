b901a321e1caa5d81599858c488289a61a0b9e34
Mark <mrkswrn@gmail.com>
2021-10-23 14:00:48 +0200

    Set logger to different level so that it stops spamming the console

--- helper/log.py
+++ helper/log.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
 handler = logging.FileHandler(filename="./logs/discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
@@ -10,6 +10,7 @@ logger.addHandler(handler)
 def log(text, print_it=True, warning=False) -> None:
     """
     Logs a specific entry to a file.
+    :param warning:
     :param text: The text to log into a file
     :param print_it: An optional parameter which helps with sorting the log
     :return: None
