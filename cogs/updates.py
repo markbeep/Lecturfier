65128b8d515dce16efe4c542ecc8a0afabc60f8d
markbeep <mrkswrn@gmail.com>
2021-12-01 11:18:19 +0100

    added ping to advent

--- cogs/updates.py
+++ cogs/updates.py
@@ -163,12 +163,12 @@ class Updates(commands.Cog):
                 self.sent_advent = False
             if month == 12 and not self.sent_advent and 1 <= day <= 25 and dt.hour == 6 and minute <= 5:
                 channel = self.bot.get_channel(910450760234467328)
-                msg = f"Good Morning <@&910433615689699388>! It's time for **Advent of Code** day #{day}!\n[*Click here to get to the challenge*](https://adventofcode.com/2021/day/{day})"
+                msg = f"Good Morning! It's time for **Advent of Code** day #{day}!\n[*Click here to get to the challenge*](https://adventofcode.com/2021/day/{day})"
                 msg += "\n||*Consider donating to AoC to have it continue next year again* <:Santawww:910435057607524352>||"
                 embed = discord.Embed(
                     description=msg,
                     color=discord.Color.red())
-                await channel.send(embed=embed)
+                await channel.send("<@&910433615689699388>", embed=embed)
                 self.sent_advent = True
 
         except AttributeError as e:
