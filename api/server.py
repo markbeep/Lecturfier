7dfe754e3a4da769d9657f94b99845a50982c5d3
Mark <mrkswrn@gmail.com>
2023-03-08 09:56:40 +0100

    removes emote ids

--- api/server.py
+++ api/server.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify
 import sqlite3
-
+import re
 
 app = Flask(__name__)
+
+
+def remove_emote_ids(phrase: str) -> str:
+    """Removes emote ids from quotes.
+    <:pog:753549087172853764> is turned into :pog: for example
+    """
+    r = re.compile(r"<:(\d|\w+):\d+>")
+    while match := r.search(phrase):
+        emote_name = r.findall(phrase)[0]
+        phrase = phrase[:match.start()] + f":{emote_name}:" + phrase[match.end():]
+    return phrase
 
 
 @app.route("/api/random-quote")
@@ -11,7 +22,7 @@ def random_quote():
     res = conn.execute("SELECT QuoteID, Quote, Name, CreatedAt, Elo FROM Quotes WHERE DiscordGuildID=747752542741725244 ORDER BY RANDOM() LIMIT 1").fetchone()
     return jsonify({
         "id": res[0],
-        "quote": res[1],
+        "quote": remove_emote_ids(res[1]),
         "name": res[2],
         "createdAt": res[3],
         "elo": res[4],
